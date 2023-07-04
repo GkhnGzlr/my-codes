3342ce454bff966ddd18c46a81593d4f83b191d5
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-07-04 20:03:58 +0200

    a

---
+++ react/class-notes/rc04_horoscope-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">Learn React</header>
+    </div>
+  );
+}
+
+export default App;
