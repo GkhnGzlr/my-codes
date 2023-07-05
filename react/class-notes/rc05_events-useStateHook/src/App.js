2a34420df9ce4390517f02dc6c7e6d472360e82e
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-07-05 22:22:43 +0200

    a

--- react/class-notes/rc05_events-useStateHook/src/App.js
+++ react/class-notes/rc05_events-useStateHook/src/App.js
@@ -1,7 +1,15 @@
+// import Events from "./1-events/Events"
+// import Counter from "./2-classComponent/Counter"
+import UseStateCounter from "./3-useState/UseStateCounter"
+import UseStateObject from "./3-useState/UseStateObject"
+
 function App() {
   return (
     <div>
-      <h1>EVENTS</h1>
+      {/* <Events />
+      <Counter /> */}
+      {/* <UseStateCounter /> */}
+      <UseStateObject />
     </div>
   )
 }
