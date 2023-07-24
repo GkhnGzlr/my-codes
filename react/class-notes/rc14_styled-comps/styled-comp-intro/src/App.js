0b145bd3a12f2d38d06cae9bb3629cb37afecf05
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-07-24 20:42:02 +0200

    button

--- react/class-notes/rc14_styled-comps/styled-comp-intro/src/App.js
+++ react/class-notes/rc14_styled-comps/styled-comp-intro/src/App.js
@@ -1,20 +1,25 @@
-import { Button, DarkButton } from "./components/Buton.styled"
-import Flex from "./components/Flex"
+import { Button, DarkButton } from "./components/Buton.styled";
+import Flex from "./components/Flex";
+import Title from "./components/Title";
 
 const App = () => {
   return (
     <>
       <Flex bg="#bebe">
-        <h1>Styled Components</h1>
+        <Title renk="white" bgRenk="red">
+          Styled Components
+        </Title>
         <Button>Tikla</Button>
         <DarkButton>Send</DarkButton>
       </Flex>
 
+      <Title renk="purple">Flexbox</Title>
+      <Title bgRenk="green">Flexbox</Title>
       <Flex bg="tomato">
         <h1>PROPS</h1>
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
