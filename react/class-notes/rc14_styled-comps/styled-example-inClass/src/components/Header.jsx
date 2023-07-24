3b278d742b5753622ba8f711074b05acd737a4f6
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-07-24 22:31:24 +0200

    A

--- react/class-notes/rc14_styled-comps/styled-example-inClass/src/components/Header.jsx
+++ react/class-notes/rc14_styled-comps/styled-example-inClass/src/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Button from "./styled/Button"
+import HeaderStyled from "./styled/HeaderStyled"
+import Image from "./styled/Image"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HeaderStyled>
+      <div>
+        <h1>The IT Career of Your Dreams Starts Here!</h1>
+        <p>
+          Clarusway is a leading international software Bootcamp. Join a micro
+          class online with other trainees and learn coding skills with a
+          highly-skilled instructor.
+        </p>
+        <Button primary>Start Your New Carier</Button>
+      </div>
+
+      <Image src="./images/hero.jpg" />
+    </HeaderStyled>
   )
 }
 
