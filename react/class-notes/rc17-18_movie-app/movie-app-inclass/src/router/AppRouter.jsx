341324297434cd4c7b17628ae45796dbbd09caab
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-08-02 16:20:05 +0200

    a

--- react/class-notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
+++ react/class-notes/rc17-18_movie-app/movie-app-inclass/src/router/AppRouter.jsx
@@ -4,19 +4,22 @@ import Main from "../pages/Main";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/details/:id" element={<MovieDetail />} />
+        <Route element={<PrivateRouter />}>
+          <Route path="/details/:id" element={<MovieDetail />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
