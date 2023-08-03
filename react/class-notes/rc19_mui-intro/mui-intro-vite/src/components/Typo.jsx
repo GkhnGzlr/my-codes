ac69db622863be8616dc4189dd57f96caf1a2529
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2023-08-03 20:16:34 +0200

    a

---
+++ react/class-notes/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -0,0 +1,16 @@
+// import { Typography,Button } from "@mui/material"
+
+import Typography from "@mui/material/Typography"
+//? Performans acisindan daha mantikli
+import Button from "@mui/material/Button"
+
+
+const Typo = () => {
+  return (
+    <div>
+      <Typography>MUI Typo</Typography>
+    </div>
+  )
+}
+
+export default Typo
